3cb947e783c0768d43344bb6ddefd3870b3ee66d
jsaravia1 <jsaravia@tata.com.gt>
2021-04-20 11:25:32 -0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Marlon Saravia"/>
   </div>
 </template>
 
